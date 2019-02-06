0cdedfcbd174d920213757b5d8325050100298a3
shrke <shreyamkela@gmail.com>
2019-02-06 13:07:55 -0800

    class and object

--- Assignments/Refresher/JavaScript/Class.js
+++ Assignments/Refresher/JavaScript/Class.js
@@ -21,12 +21,15 @@ class Transport {
 const Car = new Transport('Car', 'Audi Q7', '5') ;
 console.log(`Type: ${Car.myType}, Name: ${Car.myName}, Capacity: ${Car.myCapacity}`);
 
-// We cannot add method  on the fly to a class derived object. But we can add a method to the class itself
+// ADD A METHOD TO A CLASS ON THE FLY:
+// We cannot add a method on the fly to a class derived object. But we can add a method to the class itself
 /* // This is incorrect
 Aeroplane.prototype.flyingAltitude = function () {
     return '45000 ft';
 }
 */
+// We can also add methods to function definitions in a similar fashion
+// But how do we add methods to objects that have been derived from classes, in JS?
 Transport.prototype.flyingAltitude = function (altitude) {
     this.altitude = altitude;
     return this.altitude;
---
+++ Assignments/Refresher/JavaScript/Object.js
@@ -0,0 +1,14 @@
+let organism = {
+    type: "Amphibian",
+    name: "Frog",
+    colour: "Green",
+    height: "3 inches",
+    weight: "1 pound",
+    count: 8456489
+}
+
+console.log(organism);
+console.log('\nAdding a property to the object.\n');
+
+organism.sound = "ribbit"; // Adding a property to the object
+console.log(organism);
