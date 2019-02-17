902d7abcbe19a605c824d4f5bb53bde65f8c1c50
shrke <shreyamkela@gmail.com>
2019-02-16 23:59:25 -0800

    user details submit validation

--- Assignments/LoginPage/validateUserUniqueness.js
+++ Assignments/LoginPage/validateUserUniqueness.js
@@ -2,5 +2,8 @@ function validateUserUniqueness() {
     var fullName = document.getElementById('name');
     var studentID = document.getElementById('sid');
     var department = document.getElementById('dept');
-    document.getElementById('result').innerHTML = `Name ${fullName.value} ID ${studentID.value} Department ${department.value}`;
+    if(fullName.match('/[A-Za-z]/') && studentID.match('/[0-9]{9}/') && department.match('/[A-Za-z]/')) {
+        document.getElementById('result').innerHTML = `Name ${fullName.value} ID ${studentID.value} Department ${department.value}`;
+    }
+    
 }
