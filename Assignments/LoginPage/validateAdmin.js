e81c2ae37391ff4ba966c67d863b6968ecfee680
shrke <shreyamkela@gmail.com>
2019-02-16 23:46:40 -0800

    submit validation

--- Assignments/LoginPage/validateAdmin.js
+++ Assignments/LoginPage/validateAdmin.js
@@ -2,12 +2,17 @@ function validateAdmin() {
     var userName = document.getElementById('username');
     var passWord = document.getElementById('password');
     if(userName.value == 'admin' && passWord.value == 'admin') {
+        console.log("Access Granted!");
         alert("Access Granted!");
+        // TAKE TO THE USER DETAILS PAGE
     } else {
-        alert("Access Denied!");
-        userName.value = ""; // If access denied, reset the fields
-        passWord.value = "";
-
+        //alert("Access Denied!"); // If we use alert then the 'Please fill out this field' validation message of HTML5 is not shown and straightaway this alert box is shown
+        console.log("Access Denied!");
+        if(userName.value != "" && passWord.value != "") { // Reset only if both the text boxes are filled
+            userName.value = ""; // If access denied, reset the fields
+            passWord.value = "";
+            alert("Access Denied!");
+        }
         // ADD COOKIES AND EXPRESS SESSION FUNCTIONALITY TO RESTRICT USER IF ACCESS DENIED
     }
 }
--- Assignments/LoginPage/validateUserUniqueness.js
+++ Assignments/LoginPage/validateUserUniqueness.js
@@ -1,13 +1,6 @@
-function validateAdmin() {
-    var userName = document.getElementById('username');
-    var passWord = document.getElementById('password');
-    if(userName.value == 'admin' && passWord.value == 'admin') {
-        alert("Access Granted!");
-    } else {
-        alert("Access Denied!");
-        userName.value = ""; // If access denied, reset the fields
-        passWord.value = "";
-
-        // ADD COOKIES AND EXPRESS SESSION FUNCTIONALITY TO RESTRICT USER IF ACCESS DENIED
-    }
+function validateUserUniqueness() {
+    var fullName = document.getElementById('name');
+    var studentID = document.getElementById('sid');
+    var department = document.getElementById('dept');
+    document.getElementById('result').innerHTML = `Name ${fullName.value} ID ${studentID.value} Department ${department.value}`;
 }
