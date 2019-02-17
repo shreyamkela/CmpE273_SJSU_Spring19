07b59b3e566ed284d8e20a6eac0c46396537d7b4
shrke <shreyamkela@gmail.com>
2019-02-17 02:17:01 -0800

    ejs templating

---
+++ Assignments/LoginPage/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const fs = require('fs');
+
+const port = 3000;
+
+var app = express(); // make an app/application server that handles requests
+
+app.set('view engine', 'ejs');
+
+// Maintain a log for all requests to the server:
+app.use((req, res, next) => { //Registering your custom express middleware. next specifies what to do when this middleware call completes. Using this we can chain different middlewares. This is async operation so next has to be called otherwise the app handlers for requests will never fire and app pauses till next is called. This middleware moniters all of the requests to our server and therefore if next is not used, our server will not respond to any request
+    var now = new Date().toString(); // To log date and time when server is pinged
+    var log = `${now}: ${req.method} ${req.url}`; // Logger - Logs whenever our server is pinged for any kind of requests. req.method is the kind of request made by the user, i.e GET, POST, etc. req.url is the url that the user visited/pinged
+    console.log(log); 
+    fs.appendFile('server.log', log + '\n', (err) => { // Log the current ping into server.log. log + '\n' is used to append new ping in nextline. Callback function is necessary for appendfile. We use error check as a callback function here. appendfile is an async func. Before node 7 async func were allowed to be made without callback but after that they have been deprecated. So we have to mention a callback func to an sync function 
+        if(err) {
+            console.log('Unable to append to server.log');
+        }
+    });
+    next(); // If next is not used, server pauses here and no request is served
+});
+
+// Main page:
+// Keep the ejs, hbs, etc view files (which are essentially all html files) in the views folder. Keep the static files like plain html files, images, etc in the public folder and configure them with express static middleware
+// Refer - https://stackoverflow.com/questions/16111386/error-cannot-find-module-html
+// Refer - https://www.geeksread.com/what-is-ejs-html-page-using-nodejs-express-framework/
+app.get('/', (req, res) => { // / is the root or landing page/route of your application/website. Here we are defining what happens when user visits the '/' page. req defines all the data/flags etc the user has sent i.e req can be set by the user input and then by analysis req we can determine what to send back. res is the response we send back
+    res.render('login.ejs'); 
+});
+
+app.listen(port, () => { // We can also pass a function as the second argument to listen. Listen to port=3000 or heroku
+    console.log(`Server is up on port ${port}`);
+});
--- Assignments/LoginPage/validateUserUniqueness.js
+++ Assignments/LoginPage/validateUserUniqueness.js
@@ -1,6 +1,6 @@
 // const fs = require('fs'); // To read and manipulate the database.json file - npm install fs
 // validateUserUniqueness is on the client side therefore require does not work. We can only require files that are on the server side. To include files on the client side, we use the 'import' keyword, as done in ReactJS which renders heavy content on frontend. Refer - https://stackoverflow.com/questions/19059580/client-on-node-uncaught-referenceerror-require-is-not-defined
-// Another option is to keep database.json on the server and manipulate it with the help of an express server.
+// Another option is to keep database.json on the server side and manipulate it with the help of an express server.
 
 // function validateUserUniqueness() {
 //     var fullName = (document.getElementById('name')).value;
@@ -34,5 +34,5 @@ function validateUserUniqueness() {
     var department = (document.getElementById('dept')).value;
     if(fullName.match(/[A-Za-z]/) && studentID.match(/[0-9]{9}/) && department.match(/[A-Za-z]/)) { // If the the patterns for all 3 fields are correct then do further processing. Else the HTML5 validation messages will be shown
             alert(`Name: ${fullName}\nStudent ID: ${studentID}\nDepartment: ${department}`);
-        }
+    }
 }
