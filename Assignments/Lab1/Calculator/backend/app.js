6a9ec7dbd62e1d9b0a4d943d6cb2884bed4a6247
shrke <shreyamkela@gmail.com>
2019-02-26 13:30:55 -0800

    successfully sending data from frontend to backend

--- Assignments/Lab1/Calculator/backend/app.js
+++ Assignments/Lab1/Calculator/backend/app.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const calculate = require("./helpers/calculate");
+const cors = require("cors");
 
 var app = express();
+
+//use cors to allow cross origin resource sharing
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 
 app.use(express.urlencoded({ extended: true })); // Through this, req.body contains the POSTed form data - body-parser functionality is available with express v4.16+ in this manner. Body parser itself was deprecated in the newer versions.
 app.use(express.json());
@@ -13,6 +17,7 @@ app.get("/", (req, res) => {
 
 app.post("/", (req, res) => {
   var query = req.body.query;
+  console.log(query);
   var result = calculate.me(query);
   console.log(result);
   res.send("Result: " + result); // Cannot send just result, which is a number, using res.send as res.send interprets numbers as status codes - https://stackoverflow.com/questions/39498601/unable-to-send-numbers-using-res-send-using-express-with-node
