8a278774af0bf99c8026a5f89d4dd32f3e234a5e
shrke <shreyamkela@gmail.com>
2019-03-01 21:25:50 -0800

    calculator alpha version complete: have to check for any missed edge cases

--- Assignments/Lab1/Calculator/backend/app.js
+++ Assignments/Lab1/Calculator/backend/app.js
@@ -3,28 +3,29 @@ const cors = require("cors");
 
 var app = express();
 
-//use cors to allow cross origin resource sharing
+//use cors to allow cross origin resource sharing. This is required to enable get and post from frontend port 3000 to this backend port 3001
 app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 
 app.use(express.urlencoded({ extended: true })); // Through this, req.body contains the POSTed form data - body-parser functionality is available with express v4.16+ in this manner. Body parser itself was deprecated in the newer versions.
 app.use(express.json());
 // From Postman, when checking the post routes, the req.body will by posted through x-www-form-urlencoded in the body tab Postman (not form-data)
 
-app.get("/", (req, res) => {
-  res.send("Render Calculator");
-});
+// app.get("/", (req, res) => {
+//   res.send("Render Calculator");
+// });
 
 app.post("/", (req, res) => {
-  var query = req.body.query;
+  let query = req.body.query;
   console.log(query);
   try {
     // if expression is invalid then eval will throw error and would send 500 error code to frontend, therefore we need a try catch to tackle invalid expressions
-    var value = eval(query); // eval function can evaluate a valid mathematical expression - https://www.geeksforgeeks.org/javascript-eval-function/
+    let value = eval(query); // eval function can evaluate a valid mathematical expression - https://www.geeksforgeeks.org/javascript-eval-function/
     console.log(value);
+    res.status(200).send({ value: value });
   } catch (e) {
-    var value = "INVALID INPUT";
-  } finally {
-    res.send("Result: " + value); // Cannot send just eval(query), which is a number, using res.send as res.send interprets numbers as status codes - https://stackoverflow.com/questions/39498601/unable-to-send-numbers-using-res-send-using-express-with-node
+    // let value = "INVALID INPUT";
+    // res.statusMessage("invalid");
+    res.status(400).send("INVALID INPUT");
   }
 });
 
--- Assignments/Lab1/Calculator/frontend/src/App.js
+++ Assignments/Lab1/Calculator/frontend/src/App.js
@@ -2,27 +2,55 @@ import React, { Component } from "react";
 import axios from "axios";
 import "./App.css";
 
+// App -> Container -> (row which has bootstrap display card/panel and below it are the rows for the digits/operators)
+
 const cardStyle = {
-  // Setting the css to be used in JSX
+  // Setting the css to be used in JSX. Card is the display where result is shown
+  // We use such a size such that character limit that can be displayed in the card is 42. Any more characters will cross the border of the card
   height: "100px",
   width: "165px"
 };
 
 const appStyle = {
+  // Setting the css to be used in JSX. App contains the calculator container
   width: "178px"
 };
 
 class App extends Component {
   state = {
-    expression: "" // Set display character limit in the panel to 36
+    expression: "", // Initially we set expression to empty. Expression is the property which is sent to the backend. eval function evaluates this expression
+    errorFlag: false // this flag is set when there is invalid expression. We need this flag so as to refresh the expression property when the expression is invalid
   };
 
-  handleEquals = () => {
-    // clicking the equals button
+  handleEquals = async () => {
+    // Uses async await. async keyword after the = is the current syntax. Previously, async keyword before the handleClick was valid syntax
     console.log("Submit Clicked!");
-    axios.post("http://localhost:3001/", {
-      query: this.state.expression
-    });
+    let data;
+
+    if (this.state.expression === "") {
+      // If expression is empty and equal is clicked, print empty
+      return;
+    }
+
+    if (this.state.errorFlag === true) {
+      // If invalid expression or equal is clicked a number of times after invalid exp, empty the expression
+      this.setState({ expression: "", errorFlag: !this.state.errorFlag });
+    }
+
+    try {
+      // fetch data from a url endpoint. We need try catch as catch block will catch the error which is sent by the backend eval function when the expression is invalid
+      data = await axios.post("http://localhost:3001/", {
+        query: this.state.expression
+      });
+      //console.log(data);
+      //console.log(data.data.value);
+      this.setState({ expression: data.data.value });
+    } catch (error) {
+      //console.log("Errors");
+      //console.log(error);
+      //console.log(error.response.data);
+      this.setState({ expression: error.response.data, errorFlag: !this.state.errorFlag }); // If error then change the error flag
+    }
   };
 
   handleClick = event => {
@@ -30,11 +58,34 @@ class App extends Component {
     // We want to send the button id of 0-9, +,-,/,* and we want to handle them all with a single handler as we only want to concat them into the previous expression
     // Refer - https://developer.mozilla.org/en-US/docs/Web/API/Event/target
     // Refer - https://www.freecodecamp.org/forum/t/react-js-passing-button-id-as-parameter-onclick-function/62301
-    console.log(event.target.id);
+    console.log(event.target.id); // event is the property of each click event and it has a target property which stores the button id. Using event.target - https://developer.mozilla.org/en-US/docs/Web/API/Event/target
     var previousExpression = this.state.expression;
-    var expression = previousExpression + event.target.id;
+
+    if (this.state.errorFlag === true) {
+      // When a button is clicked after and invalid input error was generated, we need to flush all of the previously stored expression
+      previousExpression = "";
+      this.setState({ errorFlag: !this.state.errorFlag });
+    }
+
+    var expression = previousExpression + event.target.id; // Concat current click will the previous expression
     console.log(expression);
     this.setState({ expression });
+  };
+
+  displayTruncated = () => {
+    // This is displayed on the display card
+    // Our card can only display 42 characters, therefore we have to truncate what we are displaying in the card if the length of expression is large, else the characters print out of the card
+    let currentExpression = this.state.expression;
+    let expressionLength = currentExpression.length;
+
+    if (expressionLength === 0) {
+      return "0";
+    } else if (expressionLength > 35) {
+      let truncatedExpression = "..." + currentExpression.substring(expressionLength - 36, expressionLength - 1);
+      return truncatedExpression;
+    } else {
+      return currentExpression;
+    }
   };
 
   render() {
@@ -44,7 +95,8 @@ class App extends Component {
           <div className="row">
             <div className="card border border-primary" style={cardStyle}>
               <div className="card-body">
-                <p className="card-title">{this.state.expression}</p>
+                <p className="card-title">{this.displayTruncated()}</p>{" "}
+                {/* Here we have used this.displayTruncated() instead of this.displayTruncated. When ever we have to print the returned value of function that is call the function, we use (), and whenever we have to use the reference that is, when we have to only execute that function, we dont use ()*/}
               </div>
             </div>
           </div>
@@ -105,6 +157,18 @@ class App extends Component {
             </button>
             <button id="+" onClick={this.handleClick} type="button" className="btn btn-outline-primary waves-effect m-1">
               +
+            </button>
+          </div>
+          <div className="row px-3">
+            <button
+              id="clear"
+              onClick={() => {
+                this.setState({ expression: "", errorFlag: false });
+              }}
+              type="button"
+              className="btn btn-outline-primary waves-effect px-5"
+            >
+              Clear
             </button>
           </div>
         </div>
