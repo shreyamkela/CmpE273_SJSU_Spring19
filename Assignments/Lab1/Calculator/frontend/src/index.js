619650183da9fb8eb27f9801d6dd811cfb49502a
shrke <shreyamkela@gmail.com>
2019-03-01 22:06:30 -0800

    find a way to reduce the delay for loading the bootstrap on the buttons when the calculator is loaded for the first time

--- Assignments/Lab1/Calculator/frontend/src/index.js
+++ Assignments/Lab1/Calculator/frontend/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap.css"; // FIXME Is importing bootstrap in index.js causing the porblem of page first showing non-bootstrap buttons for a splitsecond and then instantly changing to the bootstrapped version. Where should we put this import so it is available everywhere?
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
